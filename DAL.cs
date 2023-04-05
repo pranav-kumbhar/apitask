5dcf9568414c367c97baa78716aa6cd6fe0917e5
pranav-kumbhar <pranavkumbhar3181@gmail.com>
2023-04-05 20:40:22 +0530

    new file added

---
+++ DAL.cs
@@ -0,0 +1,35 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class DAL
+    {
+       public static async void GetEntries()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://api.publicapis.org/entries");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var apiData = JsonConvert.DeserializeObject<Root>(responseContent);
+            var data = apiData.entries.ToList(); 
+            string filePath = @"E:\Temp\Entries.txt";
+            Console.WriteLine("Record insert Starts...");
+            foreach (var item in data)
+            {
+                string texts = $" API: {item.API} \n Description: {item.Description} \n Auth:{item.Auth} \n HTTPS: {item.HTTPS} \n Cors:{item.Cors} \n Link:{item.Link} \n Category: {item.Category} \n \n \n \n";            
+                File.AppendAllText(filePath, texts);               
+            }
+            string readText = File.ReadAllText(filePath);
+            Console.WriteLine(readText);
+            Console.WriteLine("Record insert Ends ...");
+        }
+    }
+}
---
+++ EntriesModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+
+    public class Entry
+    {
+        public string API { get; set; }
+        public string Description { get; set; }
+        public string Auth { get; set; }
+        public bool HTTPS { get; set; }
+        public string Cors { get; set; }
+        public string Link { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class Root
+    {
+        public int count { get; set; }
+        public List<Entry> entries { get; set; }
+    }
+}
